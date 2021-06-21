b470c4c7380e9d48c1ff2ec6d05be8986f866211
Alina <alina.lm@mail.ru>
2021-06-21 23:18:15 +0300

    fix canceling request

--- src/store/epics/hints.ts
+++ src/store/epics/hints.ts
@@ -1,21 +1,23 @@
 import {Epic, ofType} from 'redux-observable';
 import {from, throwError} from 'rxjs';
-import {catchError, filter, map, mergeMap, takeUntil, tap} from 'rxjs/operators';
-import axios from "axios";
+import {catchError, filter, map, switchMap, takeUntil} from 'rxjs/operators';
+import axios, {CancelTokenSource} from "axios";
 
 import {GET_HINTS, getHintsSuccess} from "../actions/hints";
 import {IGetHints} from "../../interfaces/hints";
 import {AppState} from "../store";
 
-
 export const hintsEpic: Epic<IGetHints, IGetHints, AppState> = action$ => {
+    let cancelToken:CancelTokenSource;
     return action$.pipe(
         ofType(GET_HINTS),
         // debounceTime(1000),
-        mergeMap(
+        switchMap(
             (action: IGetHints) => {
-                const CancelToken = axios.CancelToken;
-                const source = CancelToken.source();
+                if (cancelToken !== undefined) {
+                    cancelToken.cancel("Cancel request");
+                }
+                cancelToken = axios.CancelToken.source();
                 return from(
                     axios.request({
                         url: "https://api.sberauto.com/searcher/suggestHints",
@@ -23,20 +25,20 @@ export const hintsEpic: Epic<IGetHints, IGetHints, AppState> = action$ => {
                         data: {
                             text: action.payload
                         },
-                        cancelToken: source.token
-                    }),
+                        cancelToken: cancelToken.token
+                    })
                 ).pipe(
                     map(response =>
                         getHintsSuccess(response.data.data.hints)
                     ),
                     takeUntil(
                         action$.pipe(
-                        filter((action: IGetHints) => action.type === 'GET_HINTS'),
-                        tap(ev => source.cancel('canceled'))
+                            filter((action: IGetHints) => action.type !== GET_HINTS),
                     )),
                     catchError((error) => throwError(error)),
                 )
             }
         ),
+        catchError((error) => throwError(error)),
     )
 }
